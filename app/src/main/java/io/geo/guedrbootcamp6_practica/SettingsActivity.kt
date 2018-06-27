ec50ad1e6d0a058feb2e14cb6398af734980e43d
Geo <geovanni.duarte.guerrero@gmail.com>
2018-06-27 11:11:16 -0500

    Launch SettingsActivity from ForecastActivity

--- app/src/main/java/io/geo/guedrbootcamp6_practica/SettingsActivity.kt
+++ app/src/main/java/io/geo/guedrbootcamp6_practica/SettingsActivity.kt
@@ -2,11 +2,32 @@ package io.geo.guedrbootcamp6_practica
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        ok_button.setOnClickListener {
+            acceptSettings()
+        }
+
+        cancell_button.setOnClickListener {
+            acceptSettings()
+        }
+
+        units_rg.check(R.id.celsius_rb)
+    }
+
+    private fun cancelSettings() {
+
+
+    }
+
+    private fun acceptSettings() {
+
+
     }
 }
