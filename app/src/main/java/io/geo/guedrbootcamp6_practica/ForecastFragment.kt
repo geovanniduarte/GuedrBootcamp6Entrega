f423eabb63a6e22eab51ce6b927574d1ef483379
Geo <geovanni.duarte.guerrero@gmail.com>
2018-06-28 17:43:44 -0500

    First fragment!!

---
+++ app/src/main/java/io/geo/guedrbootcamp6_practica/ForecastFragment.kt
@@ -0,0 +1,116 @@
+package io.geo.guedrbootcamp6_practica
+
+import android.app.Activity
+import android.app.Fragment
+import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.design.widget.Snackbar
+import android.view.*
+import kotlinx.android.synthetic.main.fragment_forecast.*
+
+class ForecastFragment: Fragment() {
+    val REQUEST_SETTINGS = 1
+    val PREFERENCE_UNITS = "UNITS"
+
+    var forecast: Forecast? = null
+        set(value) {
+            field = value
+            if (value != null) {
+                forecast_image.setImageResource(value.icon)
+                forecast_description.text = value.description
+
+                updateTemperatureView()
+                humidity.text = getString(R.string.humidity_format, value.humidity)
+            }
+        }
+    //variable que indica las unidades en las que queremos la temperatura, por defecto celsius
+    val units : TemperatureUnit
+        get () = when ( PreferenceManager.getDefaultSharedPreferences(activity)
+                .getInt(PREFERENCE_UNITS, TemperatureUnit.CELSIUS.ordinal)) {
+            TemperatureUnit.CELSIUS.ordinal -> TemperatureUnit.CELSIUS
+            else -> TemperatureUnit.FAHRENHEIT
+        }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        var root = inflater?.inflate(R.layout.fragment_forecast, container, false)
+
+        return  root!!
+
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        forecast = Forecast(25f,
+                10f,
+                35f,
+                "Soleado con alguna nube",
+                R.drawable.ico_01)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.activity_forecast, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.menu_show_settings -> {
+                //lanzamos la pantalla de ajustes
+                startActivityForResult(SettingsActivity.intent(activity, units),
+                        REQUEST_SETTINGS)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode) {
+            REQUEST_SETTINGS -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    //Volvemos de settings con datos sobre las unidades elegidads por el usuario.
+                    val newUnits = data.getSerializableExtra(SettingsActivity.EXTRA_UNITS) as TemperatureUnit
+                    val oldUnits = units
+                    //Guardamos las preferenciaas del usuario
+                    PreferenceManager.getDefaultSharedPreferences(activity)
+                            .edit()
+                            .putInt(PREFERENCE_UNITS, newUnits.ordinal)
+                            .apply()
+
+                    //Actualizo la interfaz con las nuevas unidades.
+                    updateTemperatureView()
+
+                    val newUnitsString = if (newUnits == TemperatureUnit.CELSIUS) getString(R.string.user_selects_celsius) else
+                        getString(R.string.user_selects_fahrenheit)
+
+                    //Toast.makeText(this, newUnitsString, Toast.LENGTH_LONG).show()
+                    Snackbar.make(view, newUnitsString, Snackbar.LENGTH_LONG)
+                            .setAction("Deshacer", View.OnClickListener {
+                                PreferenceManager.getDefaultSharedPreferences(activity)
+                                        .edit()
+                                        .putInt(PREFERENCE_UNITS, oldUnits.ordinal)
+                                        .apply()
+                                updateTemperatureView()
+                            })
+                            .show()
+                }
+            }
+        }
+    }
+
+    // Aquí actualizaremos la interfaz con las temperaturas
+    fun updateTemperatureView() {
+        val unitsString = units2String()
+        max_temp.text = getString(R.string.max_temp_format, forecast?.getMaxTemp(units), unitsString)
+        min_temp.text = getString(R.string.min_temp_format, forecast?.getMinTemp(units), unitsString)
+    }
+
+    fun units2String() = if (units == TemperatureUnit.CELSIUS) "ºC" else "F"
+}
