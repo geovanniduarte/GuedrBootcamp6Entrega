54fc3875d6d17063868afc27536bd96b04345ae0
Geo <geovanni.duarte.guerrero@gmail.com>
2018-06-28 14:19:18 -0500

    tests and validation range humidity in Forecast class

--- app/src/main/java/io/geo/guedrbootcamp6_practica/Forecast.kt
+++ app/src/main/java/io/geo/guedrbootcamp6_practica/Forecast.kt
@@ -8,6 +8,12 @@ data class Forecast(private val maxTemp: Float,private val minTemp: Float, val h
 
     protected fun toFahrenheit(celsius: Float) = celsius * 1.8f + 32
 
+    init {
+        if (humidity !in 0f..100f) {
+            throw IllegalArgumentException("Humidity should be betwenn 0f and 100f")
+        }
+    }
+
     fun getMaxTemp(units: TemperatureUnit) =
         when (units) {
             TemperatureUnit.CELSIUS -> maxTemp
