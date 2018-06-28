3da8ee28f079c4057a015c032dbeceda27b5ca82
Geo <geovanni.duarte.guerrero@gmail.com>
2018-06-28 13:53:00 -0500

    Example usage SnackBar with action buttons.

--- app/src/main/java/io/geo/guedrbootcamp6_practica/Forecast.kt
+++ app/src/main/java/io/geo/guedrbootcamp6_practica/Forecast.kt
@@ -1,5 +1,25 @@
 package io.geo.guedrbootcamp6_practica
 
-data class Forecast(val maxTemp: Float, val minTemp: Float, val humidity: Float, val description: String, val icon: Int) {
+enum class TemperatureUnit {
+    CELSIUS, FAHRENHEIT
+}
+
+data class Forecast(private val maxTemp: Float,private val minTemp: Float, val humidity: Float, val description: String, val icon: Int) {
+
+    protected fun toFahrenheit(celsius: Float) = celsius * 1.8f + 32
+
+    fun getMaxTemp(units: TemperatureUnit) =
+        when (units) {
+            TemperatureUnit.CELSIUS -> maxTemp
+            TemperatureUnit.FAHRENHEIT -> toFahrenheit(maxTemp)
+        }
+
+
+    fun getMinTemp(units: TemperatureUnit)  =
+        when(units) {
+            TemperatureUnit.CELSIUS -> minTemp
+            TemperatureUnit.FAHRENHEIT -> toFahrenheit(minTemp)
+        }
+
 
 }
--- app/src/main/java/io/geo/guedrbootcamp6_practica/SettingsActivity.kt
+++ app/src/main/java/io/geo/guedrbootcamp6_practica/SettingsActivity.kt
@@ -1,10 +1,29 @@
 package io.geo.guedrbootcamp6_practica
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_settings.*
 
+
 class SettingsActivity : AppCompatActivity() {
+
+    companion object {
+
+        val EXTRA_UNITS = "EXTRA_UNITS"
+
+        fun intent(context: Context, initialUnits: TemperatureUnit) : Intent {
+            var intent = Intent(context, SettingsActivity::class.java)
+            intent.putExtra(EXTRA_UNITS, initialUnits)
+            return intent
+        }
+    }
+
+    val initialUnits by lazy {
+        intent.getSerializableExtra(EXTRA_UNITS)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +36,30 @@ class SettingsActivity : AppCompatActivity() {
         cancell_button.setOnClickListener {
             acceptSettings()
         }
+        units_rg.check(if (initialUnits == TemperatureUnit.CELSIUS) R.id.celsius_rb else R.id.farenheit_rb)
+        //Decidimos que radiobutton debe estar marcado en funcion de initialUnits
+//        if (initialUnits == TemperatureUnit.CELSIUS) {
+//            units_rg.check(R.id.celsius_rb)
+//        } else {
+//            units_rg.check(R.id.farenheit_rb)
+//        }
 
-        units_rg.check(R.id.celsius_rb)
+
     }
 
     private fun cancelSettings() {
-
-
+        setResult(Activity.RESULT_CANCELED)
+        finish()
     }
 
     private fun acceptSettings() {
-
-
+        //Creamos los datos de regreso, en este caso las unidades elegidas
+        val returnIntent = Intent()
+        when (units_rg.checkedRadioButtonId) {
+            R.id.celsius_rb -> returnIntent.putExtra(EXTRA_UNITS, TemperatureUnit.CELSIUS)
+            R.id.farenheit_rb -> returnIntent.putExtra(EXTRA_UNITS, TemperatureUnit.FAHRENHEIT)
+        }
+        setResult(Activity.RESULT_OK, returnIntent)
+        finish()
     }
 }
